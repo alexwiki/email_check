de6200b560d59be21968ec026d97d1647afd72b7
Alex Wiki <alexstoubis@gmail.com>
2022-07-01 15:34:50 +0200

    Added json parser

--- email_checker.py
+++ email_checker.py
@@ -1,13 +1,31 @@
 import re
+import json
+import os
 from zenlog import log
 
 def main():
 
-    emails={"Alex":"alex@mail.com","Kostas":"kostas@mail.com"}
+    check_json('/json_data/MOCK_DATA.json')
+    #simple_manual_check()
+def check_json(file):
 
+    with open('./json_data/MOCK_DATA.json','r') as f:
+        data=json.load(f)
+        for line in data:
+            print(line)
+            log.warning("candidate e-mail: "+line["email"])
+            try: 
+                re.match(".+?@\w.+?\.\w{2,3}",line["email"])
+                log.info("RegEx matched")
+            except:
+                log.error("Failed to match regEx: "+str(line))   
+
+def simple_manual_check():    
+    emails={"Alex":"alex@mail.com","Kostas":"kostas@mail.com","Fake":"fake@mail.c"}
+    
     for key in emails:
         try: 
-            re.match(".+?@\w.+?\.\w.+",key)
+            re.match(".+?@\w.+?\.\w{2,3}",key)
             log.info("RegEx matched")
         except:
             log.error("Failed to match regEx: "+str(key))   
