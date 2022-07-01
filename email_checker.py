5124fa152616afa0420ff7134fe9d4c5e6b60cd1
Alex Wiki <alexstoubis@gmail.com>
2022-07-01 14:02:58 +0200

    First working RegEx with manual entries of e-mails

---
+++ email_checker.py
@@ -0,0 +1,17 @@
+import re
+import os
+from zenlog import log
+
+def main():
+
+    emails={"Alex":"alex@mail.com","Kostas":"kostas@mail.com"}
+
+    for key in emails:
+        try: 
+            re.match(".+?@\w.+?\.\w.+",key)
+            log.info("RegEx matched")
+        except:
+            log.error("Failed to match regEx: "+str(key))   
+
+
+main()
